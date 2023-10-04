8a08a342b3d776b35e09073d3b20836445525ecc
Clayton <persinger.clayton@gmail.com>
2023-10-04 12:10:35 -0700

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import HomePage from "./components/homePage"
+import NavBar from "./components/NavBar"
+
+import './styles/App.css'
+
+const App = () => {
+ 
+  return (
+    <>
+      <NavBar />
+      <HomePage />
+    </>
+  )
+}
+
+
+export default App
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,21 @@
+import '../styles/NavBar.css'
+
+
+const NavBar = () => {
+  return (
+    <>
+      <nav className="nav">
+        <div className="title">
+          <h1>Clayton Persinger</h1>
+        </div>
+        <div className="info">
+          <p>About</p>
+          <p>Projects</p>
+          <p>Blog</p>
+        </div>
+      </nav>
+    </>
+  )
+}
+
+export default NavBar
