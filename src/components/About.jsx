955f634678de8413b4c913e060091ec48fea9a18
Clayton <persinger.clayton@gmail.com>
2023-10-06 17:12:30 -0700

    Update about page with selfie

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,7 +4,8 @@ const About = () => {
 
   return (
     <div id="about">
-      <div>
+      <div className="user-card">
+        <img src="../assets/selfie.jpg" alt="selfie"/>
         <p>I am a full-stack developer with five years experience leading projects and organizing people in the building automation industry. My passion for modern web technologies like JavaScript, React, Node, and Ruby drives me to continuously improve my web development abilities. During my free time, I learn new technologies by working on personal projects ranging from simple web games to complex API work. I aim to become a leader in the forefront of web development and create scalable and effective systems.</p>
       </div>
    </div> 
