8a4f50e545d951629cd80ca063f762620e7117e2
Clayton <persinger.clayton@gmail.com>
2023-10-08 20:49:12 -0700

    Add more info for homepage

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,9 +1,16 @@
-
+import '../styles/HomePage.css'
 
 const HomePage = () => {
 
   return (
-    <h1>Hello world!</h1>
+    <div id="home-page">
+      <div className="container">
+        <p className="intro">Hi, my name is</p>
+        <h1 className="name">Clayton Persinger.</h1>
+        <h2 className="info">I am a full-stack web developer.</h2>
+        <p className="description">I was in the building automation industry for 5 years and descided to make a switch to create amazing applications.</p>
+      </div>
+    </div>
   )
   
 }
