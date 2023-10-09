fd429c720a659c68381cd436457da78cf1c87470
Clayton <persinger.clayton@gmail.com>
2023-10-08 18:15:13 -0700

    Import and add image to about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,14 +1,19 @@
 import { Form } from "react-router-dom";
+import selfie from "../assets/selfie.jpg"
+
+import '../styles/About.css'
 
 const About = () => {
 
   return (
     <div id="about">
       <div className="user-card">
-        <img src="../assets/selfie.jpg" alt="selfie"/>
-        <p>I am a full-stack developer with five years experience leading projects and organizing people in the building automation industry. My passion for modern web technologies like JavaScript, React, Node, and Ruby drives me to continuously improve my web development abilities. During my free time, I learn new technologies by working on personal projects ranging from simple web games to complex API work. I aim to become a leader in the forefront of web development and create scalable and effective systems.</p>
+        <img src={selfie} alt="selfie" className="img"/>
+        <h2>Clayton Persinger</h2>
+        <h3>Full-Stack Developer</h3>
+        <p className="user-description">I am a full-stack developer with five years experience leading projects and organizing people in the building automation industry. My passion for modern web technologies like JavaScript, React, Node, and Ruby drives me to continuously improve my web development abilities. During my free time, I learn new technologies by working on personal projects ranging from simple web games to complex API work. I aim to become a leader in the forefront of web development and create scalable and effective systems.</p>
       </div>
-   </div> 
+   </div>  
   )
 }
 
