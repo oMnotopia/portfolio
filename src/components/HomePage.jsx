7425afd4f166805be4dcf96095a31c0b088ae64c
Clayton <persinger.clayton@gmail.com>
2023-10-09 13:04:54 -0700

    Change class names

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -7,8 +7,8 @@ const HomePage = () => {
       <div className="container">
         <p className="intro">Hi, my name is</p>
         <h1 className="name">Clayton Persinger.</h1>
-        <h2 className="info">I am a full-stack web developer.</h2>
-        <p className="description">I was in the building automation industry for 5 years and descided to make a switch to create amazing applications.</p>
+        <h2 className="description-1">I am a full-stack web developer.</h2>
+        <p className="description-2">I was in the building automation industry for 5 years and descided to make a switch to create amazing applications.</p>
       </div>
     </div>
   )
