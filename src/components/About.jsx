640a4840105786e510ae581d089d9b2d8c26d254
Clayton <persinger.clayton@gmail.com>
2023-10-11 12:52:20 -0700

    change div tags to section tags

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,14 +5,14 @@ import '../styles/About.css'
 const About = () => {
 
   return (
-    <div id="about">
+    <section id="about">
       <div className="user-card">
         <img src={selfie} alt="selfie" className="img"/>
         <h2>Clayton Persinger</h2>
         <h3>Full-Stack Developer</h3>
         <p className="user-description">I am a full-stack developer with five years experience leading projects and organizing people in the building automation industry. My passion for modern web technologies like JavaScript, React, Node, and Ruby drives me to continuously improve my web development abilities. During my free time, I learn new technologies by working on personal projects ranging from simple web games to complex API work. I aim to become a leader in the forefront of web development and create scalable and effective systems.</p>
       </div>
-   </div>  
+   </section>  
   )
 }
 
