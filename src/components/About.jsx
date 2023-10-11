5029b056f8d8c57cc4a6b7643309050b0740ea01
Clayton <persinger.clayton@gmail.com>
2023-10-11 13:19:43 -0700

    add extra div container

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,11 +6,13 @@ const About = () => {
 
   return (
     <section id="about">
-      <div className="user-card">
-        <img src={selfie} alt="selfie" className="img"/>
-        <h2>Clayton Persinger</h2>
-        <h3>Full-Stack Developer</h3>
-        <p className="user-description">I am a full-stack developer with five years experience leading projects and organizing people in the building automation industry. My passion for modern web technologies like JavaScript, React, Node, and Ruby drives me to continuously improve my web development abilities. During my free time, I learn new technologies by working on personal projects ranging from simple web games to complex API work. I aim to become a leader in the forefront of web development and create scalable and effective systems.</p>
+      <div className="user-card-container">
+        <div className="user-card">
+          <img src={selfie} alt="selfie" className="img"/>
+          <h2>Clayton Persinger</h2>
+          <h3>Full-Stack Developer</h3>
+          <p className="user-description">I am a full-stack developer with five years experience leading projects and organizing people in the building automation industry. My passion for modern web technologies like JavaScript, React, Node, and Ruby drives me to continuously improve my web development abilities. During my free time, I learn new technologies by working on personal projects ranging from simple web games to complex API work. I aim to become a leader in the forefront of web development and create scalable and effective systems.</p>
+        </div>
       </div>
    </section>  
   )
