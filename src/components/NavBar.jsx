8cf5c084a0cad567811c8db3d0a5a3ec89f9745a
Clayton <persinger.clayton@gmail.com>
2023-10-11 12:52:09 -0700

    change navLinks to a tags for hash router

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,21 +1,29 @@
 import '../styles/NavBar.css'
 
-import { NavLink, Outlet } from "react-router-dom"
+import About from './About'
+import HomePage from './homePage'
+
+import {NavLink, Outlet } from "react-router-dom"
 
 const NavBar = () => {
   return (
     <>
       <nav className="nav">
         <div className="title">
-          <NavLink to={`/`} >Clayton Persinger</NavLink>
+          {/* <NavLink to={`/`} >Clayton Persinger</NavLink> */}
+          <a href="/">Clayton Persinger</a>
         </div>
         <div className="info">
-          <NavLink to={`/about`} >About</NavLink>
-          <NavLink to={`/projects`} >Projects</NavLink>
+          <a href="#about">About</a>
+          <a href="#projects">Projects</a>
+          {/* <HashLink to="about" >About</HashLink>
+          <NavLink to={`/projects`} >Projects</NavLink> */}
           {/* <NavLink to={`#`} >Blog</NavLink> */}
         </div>
       </nav>
-      <Outlet />
+      {/* <HomePage />
+      <About /> */}
+      {/* <Outlet /> */}
     </>
   )
 }
