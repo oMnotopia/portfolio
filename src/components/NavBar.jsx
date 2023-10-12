5d72f78f4a7e088170b670236ff5fafe184f92c4
Clayton <persinger.clayton@gmail.com>
2023-10-12 13:40:19 -0700

    Remove old routes from routerprovider

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,30 +1,16 @@
 import '../styles/NavBar.css'
 
-import About from './About'
-import HomePage from './homePage'
-
-import {NavLink, Outlet } from "react-router-dom"
-
 const NavBar = () => {
   return (
-    <>
-      <nav className="nav">
+    <nav id="nav-bar">
         <div className="title">
-          {/* <NavLink to={`/`} >Clayton Persinger</NavLink> */}
           <a href="/">Clayton Persinger</a>
         </div>
         <div className="info">
           <a href="#about">About</a>
           <a href="#projects">Projects</a>
-          {/* <HashLink to="about" >About</HashLink>
-          <NavLink to={`/projects`} >Projects</NavLink> */}
-          {/* <NavLink to={`#`} >Blog</NavLink> */}
-        </div>
-      </nav>
-      {/* <HomePage />
-      <About /> */}
-      {/* <Outlet /> */}
-    </>
+        </div>  
+    </nav>
   )
 }
 
