f9d0308b80f93748ad27a432bec983fece1b85b7
Clayton <persinger.clayton@gmail.com>
2023-10-12 14:16:22 -0700

    Create html for projects page

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,7 +1,43 @@
+import ResourceWallMP from '../assets/resource-wall.png'
+
 const Projects = () => {
   return (
     <section id="projects">
-      <h1>Projects page</h1>
+
+      <div className="wrapper">
+        <div className="accordion">
+          <div className="accordion-panel">
+            <h2 id="panel1-heading">
+              <button
+                className="accordion-trigger"
+                aria-controls="panel1-content"
+                aria-expanded="true"
+              >
+                <span id="panel1-title">Boating</span> 
+                <svg aria-hidden="true" className="accordion-icon"></svg>
+              </button>
+            </h2>
+
+            <div
+              className="accordion-content"
+              id="panel1-content"
+              aria-labelledby="panel1-heading"
+              aria-hidden="false"
+              role="region"
+            >
+              <p>
+                LOREM IPSUM 1
+              </p>
+              <img
+                class="accordion-image"
+                src={ResourceWallMP}
+                alt="resource wall main page"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+
     </section>
   )
 }
