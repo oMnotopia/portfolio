39e500b72f8ac842e13083a7c380fe0a9aaa00f6
Omnotopia <persinger.clayton@gmail.com>
2024-03-05 12:38:46 -0800

    Fix spelling mistake

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -8,11 +8,11 @@ const HomePage = () => {
         <p className="intro">Hi, my name is</p>
         <h1 className="name">Clayton Persinger.</h1>
         <h2 className="description-1">I am a full-stack web developer.</h2>
-        <p className="description-2">I was in the building automation industry for 5 years and descided to make a switch to create amazing applications.</p>
+        <p className="description-2">I was in the building automation industry for 5 years and decided to make a switch to create amazing applications.</p>
       </div>
     </div>
   )
-  
+
 }
 
 export default HomePage;
